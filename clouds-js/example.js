0bd625b99f6ef0ca95d4f2591164e884b0072a1b
funkysi1701 <funkysi1701@gmail.com>
2015-01-14 23:11:06 +0000

    jquery now works on elements created by itself TODO: remove duplication of code

--- clouds-js/example.js
+++ clouds-js/example.js
@@ -1,22 +1,40 @@
 $('button').text('Add Folder');
-$('button').on('click', function() {
-	$('table').append('<tr><td><input type="checkbox"></td><td><img src=\"imgs/e-fld.png\" /></td><td>Filename</td><td>10KB</td></tr>');
-});
+
+	$('tbody tr').on('mouseenter', function() {
+		$(this).addClass('selected-blue');
+	});
+	$('tr').on('mouseleave', function() {
+		$(this).removeClass('selected-blue');
+	});
 
 $('tbody tr').on('click', function() {
-		if($(this).children().children().is(':checked')){
-			$(this).removeClass('selected-red');
-			$(this).children().children().prop('checked',false);
+	if($(this).children().children().is(':checked')){
+		$(this).removeClass('selected-red');
+		$(this).children().children().prop('checked',false);
+	}
+	else{
+		$(this).addClass('selected-red');
+		$(this).children().children().prop('checked',true);
 		}
-		else{
-			$(this).addClass('selected-red');
-			$(this).children().children().prop('checked',true);
-		}
-});
+	});
 
-$('tbody tr').on('mouseenter', function() {
+$('button').on('click', function() {
+	$('table').prepend('<tr><td><input type="checkbox"></td><td><img src=\"imgs/e-fld.png\" /></td><td>Filename</td><td>10KB</td></tr>');
+	$('tbody tr').on('mouseenter', function() {
 		$(this).addClass('selected-blue');
-});
-$('tr').on('mouseleave', function() {
+	});
+	$('tr').on('mouseleave', function() {
 		$(this).removeClass('selected-blue');
+	});
+
+$('tbody tr').on('click', function() {
+	if($(this).children().children().is(':checked')){
+		$(this).removeClass('selected-red');
+		$(this).children().children().prop('checked',false);
+	}
+	else{
+		$(this).addClass('selected-red');
+		$(this).children().children().prop('checked',true);
+		}
+	});
 });
